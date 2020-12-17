b95ea10fb626e8e60e46b87d310e71bafab2722c
qiagu <guqiang01@gmail.com>
2020-12-17 11:38:26 -0800

    Refactor CycSession.add_sample_complex

--- cycif_db/cyc_session.py
+++ cycif_db/cyc_session.py
@@ -43,10 +43,6 @@ class CycSession(Session):
         -----------
         sample: str, dict or Sample object.
             Sample name or dict to build a Sample object.
-
-        Returns
-        ----------
-        An object of Sample.
         """
         if isinstance(sample, str):
             sample = Sample(name=sample)
@@ -58,36 +54,41 @@ class CycSession(Session):
         self.add(sample)
         if not self.autoflush:
             self.flush()
-        sample = self.get_sample_by_name(sample.name)
-        assert sample
         log.info("Added sample {}.".format(repr(sample)))
-        return sample
 
-    def add_cells(self, sample, cells, **kwargs):
+    def insert_cells_mappings(self, sample, cells, **kwargs):
         """ Insert cell quantification data into cells table.
 
         Parameters
         ----------
-        sample: Sample object.
+        sample: str or Sample object.
             The parent of cells.
         cells: str or pandas.DataFrame object.
             If str, it's path string to a csv file.
         kwargs: keywords parameter.
             Addtional parameters used `pd.read_csv`.
-
-        Returns
-        ----------
-        None.
         """
         if isinstance(cells, str):
             cells = pd.read_csv(cells)
         elif not isinstance(cells, DataFrame):
             raise ValueError("Unsupported datatype for cells!")
 
+        if isinstance(sample, Sample):
+            sample_id = sample.id
+            assert sample_id
+        elif isinstance(sample, str):
+            sample_id = self.query(Sample.id).filter(
+                func.lower(Sample.name) == sample.lower()).first()
+            assert sample_id, (f"A Sample that matches `{sample}` doesn't seem"
+                               " to exist in database!")
+            sample_id = sample_id[0]
+        else:
+            raise ValueError("Unsupported datatype for sample.")
+
         cells.columns = cells.columns.map(header_to_dbcolumn)
         cell_obs = cells.to_dict('records')
         for ob in cell_obs:
-            ob['sample_id'] = sample.id
+            ob['sample_id'] = sample_id
         self.bulk_insert_mappings(Cell, cell_obs)
         if not self.autoflush:
             self.flush()
@@ -100,10 +101,6 @@ class CycSession(Session):
         -----------
         marker: str, dict or Marker object.
             Marker name (string) or dict to build a Marker object.
-
-        Returns
-        ----------
-        An object of Marker.
         """
         if isinstance(marker, str):
             marker = Marker(name=marker)
@@ -115,42 +112,15 @@ class CycSession(Session):
         self.add(marker)
         if not self.autoflush:
             self.flush()
-        marker = self.get_marker_by_name(marker.name)
-        assert marker
         log.info("Added marker {}.".format(repr(marker)))
-        return marker
 
-    def get_or_create_marker_by_name(self, marker_name):
-        """ Fetch a Marker object by name from markers table.
-        if fails, create one instead.
-
-        Parameters
-        ----------
-        marker_name: str.
-            The name of marker to query.
-
-        Returns
-        ----------
-        An object of Marker.
-        """
-        marker = self.get_marker_by_name(marker_name)
-        if marker:
-            log.info("Successfully fetch a martching marker %s."
-                     % repr(marker))
-        else:
-            log.info(f"No matching marker found for `{marker_name}`. Create "
-                     "one instead...")
-            marker = self.add_marker(marker_name)
-
-        return marker
-
-    def add_sample_markers(self, sample, markers):
+    def insert_sample_markers(self, sample, markers):
         """ Insert sample marker association into database.
 
         Parameters
         ----------
-        sample: Sample object.
-            The parent of cells.
+        sample: str or Sample object.
+            The parent of markers.
         markers: str or pandas.DataFrame object.
             If str, it's path string to a csv file.
         kwargs: keywords parameter.
@@ -165,11 +135,21 @@ class CycSession(Session):
         elif not isinstance(markers, DataFrame):
             raise ValueError("Unsupported datatype for markers!")
 
+        if isinstance(sample, Sample):
+            sample_id = sample.id
+        elif isinstance(sample, str):
+            sample_id = self.query(Sample.id).filter_by(name=sample).first()
+            assert sample_id, (f"A Sample that matches `{sample}` doesn't seem"
+                               " to exist in database!")
+            sample_id = sample_id[0]
+        else:
+            raise ValueError("Unsupported datatype for sample.")
+
         associates = []
         for i, row in markers.iterrows():
             marker = self.get_or_create_marker_by_name(row['marker_name'])
             asso = {
-                'sample_id': sample.id,
+                'sample_id': sample_id,
                 'marker_id': marker.id,
                 'channel_number': row['channel_number'],
                 'cycle_number': row['cycle_number']
@@ -179,7 +159,7 @@ class CycSession(Session):
         self.bulk_insert_mappings(Sample_Marker_Association, associates)
         if not self.autoflush:
             self.flush()
-        log.info("Added %d records of sample marker association!"
+        log.info("Added %d entries of sample marker association!"
                  % len(associates))
 
     def add_sample_complex(self, sample, cells, markers, **kwargs):
@@ -202,18 +182,48 @@ class CycSession(Session):
         check_feature_compatiblity(cells)
 
         try:
-            sample = self.add_sample(sample)
-            self.add_cells(sample, cells, **kwargs)
-            self.add_sample_markers(sample, markers, **kwargs)
+            if not isinstance(sample, Sample):
+                self.add_sample(sample)
+            if isinstance(sample, dict):
+                sample = sample['name']
+            self.insert_cells_mappings(sample, cells, **kwargs)
+            self.insert_sample_markers(sample, markers, **kwargs)
             self.commit()
             log.info("Adding sample complex completed!")
         except Exception:
             self.rollback()
             raise
+        finally:
+            self.close()
 
     ###################################################
-    #              Data Qquery
+    #              Data Query
     ###################################################
+    def get_or_create_marker_by_name(self, marker_name):
+        """ Fetch a Marker object by name from markers table.
+        if fails, create one instead.
+
+        Parameters
+        ----------
+        marker_name: str.
+            The name of marker to query.
+
+        Returns
+        ----------
+        An object of Marker.
+        """
+        marker = self.get_marker_by_name(marker_name)
+        if marker:
+            log.info("Successfully fetched a martching marker %s."
+                     % repr(marker))
+        else:
+            log.info("No matching marker found for %s. Create one instead..."
+                     % marker_name)
+            self.add_marker(marker_name)
+            marker = self.get_marker_by_name(marker_name)
+
+        return marker
+
     def get_sample_by_name(self, sample_name):
         """ Query  samples by name.
 
@@ -224,9 +234,10 @@ class CycSession(Session):
 
         Returns
         -------
-        None or an object of Sample.
+        Sample object or None.
         """
-        sample = self.query(Sample).filter_by(name=sample_name).first()
+        sample = self.query(Sample).filter(
+            func.lower(Sample.name) == sample_name.lower()).first()
         return sample
 
     def get_marker_by_name(self, marker_name):
@@ -239,7 +250,7 @@ class CycSession(Session):
 
         Returns
         -------
-        None or an object of Marker.
+        Marker object or None.
         """
         # TODO Make header_to_dbcolumn query.
         marker = self.query(Marker).filter(
