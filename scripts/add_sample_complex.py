15f285c073c8dc4a9125366039c095b3c450900f
qiagu <guqiang01@gmail.com>
2021-01-12 15:06:52 -0800

    Improve add_sample_complex script to take folder argument

--- scripts/add_sample_complex.py
+++ scripts/add_sample_complex.py
@@ -18,29 +18,74 @@ log = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    'sample', type=str,
-    help="Sample name and annotation separated by `;`.")
+    'sample', type=str, nargs='?',
+    help="Sample name and annotation separated by `--`.")
 parser.add_argument(
-    'cells', type=str,
+    'cells', type=str, nargs='?',
     help="The path to cells quantification data, in csv.")
 parser.add_argument(
-    'markers', type=str,
+    'markers', type=str, nargs='?',
     help="The path to markers used for the sample, in csv.")
+parser.add_argument(
+    '--dir', '-d', dest='dir', type=str, required=False,
+    help=("As an alternative input option, a folder containing bother cells "
+          "and markers csv overrides other positional arguments. The folder "
+          "name will be used as arguments for sample, separated by `--`.")
+)
 
 args = parser.parse_args()
 
-sample_args = args.sample.split(';', 1)
-if len(sample_args) == 1:
-    sample = sample_args[0]
+dir = args.dir
+
+sample_annotation = ""
+
+if dir:
+    folder = pathlib.Path(dir)
+    log.info("Use folder: %s", str(folder))
+
+    sample_args = folder.name
+
+    cells_path, markers_path = '', ''
+    for fl in folder.iterdir():
+        fl_name = fl.name.lower()
+        if 'quantification' in fl_name:
+            cells_path = str(fl.absolute())
+        elif 'markers.csv' in fl_name:
+            markers_path = str(fl.absolute())
+        elif fl.stem == 'annotation':
+            with open(fl.absolute(), 'r') as fp:
+                sample_annotation = fp.read()
+
+    if not cells_path:
+        raise Exception("Couldn't find a file having `quantification` in "
+                        "name!")
+    if not markers_path:
+        raise Exception("Couldn't find a file having `markers.csv` in "
+                        "name!")
 else:
-    sample = dict(name=sample_args[0].strip(),
-                  annotation=sample_args[1].strip())
-log.info("The sample name: {}.".format(sample))
+    if not args.sample:
+        raise Exception("Positional argument `sample` was required or "
+                        "use `--dir` option!")
+    sample_args = args.sample
 
-cells_path = str(pathlib.Path(args.cells).absolute())
+    if not args.cells:
+        raise Exception("Positional argument `cells` was required!")
+    cells_path = str(pathlib.Path(args.cells).absolute())
+
+    if not args.markers:
+        raise Exception("Positional argument `markers` was required!")
+    markers_path = str(pathlib.Path(args.markers).absolute())
+
+sample_args = sample_args.split('--', 1)
+sample = dict(name=sample_args[0].strip())
+if len(sample_args) == 2:
+    sample_annotation = \
+        sample_annotation.strip() + '\n' + sample_args[1].strip()
+if sample_annotation:
+    sample['annotation'] = sample_annotation.strip()
+
+log.info("The sample info: {}.".format(sample))
 log.info(f"The path to Cells: {cells_path}.")
-
-markers_path = str(pathlib.Path(args.markers).absolute())
 log.info(f"The path to Markers: {markers_path}.")
 
 with CycSession() as csess:
